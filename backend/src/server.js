070acbd96f85e5922354f6adee8086d759a3070b
kashishy24 <kashishy2403.com>
2026-03-13 16:35:55 +0530

    change frontend url

--- backend/src/server.js
+++ backend/src/server.js
@@ -21,7 +21,13 @@ const io = new Server(httpServer, {
   cors: { origin: [process.env.CLIENT_ORIGIN, 'http://localhost:5173'], methods: ['GET', 'POST'] },
 });
 
-app.use(cors({ origin: [process.env.CLIENT_ORIGIN, 'http://localhost:5173'], credentials: true }));
+app.use(cors({
+  origin: [
+    process.env.CLIENT_ORIGIN,
+    "http://localhost:5173"
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
